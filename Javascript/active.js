9b2db480ba0792d43303848d1e2a247446a10eb1
SrijanDahal <dahalsr@warhawks.ulm.edu>
2025-02-27 13:27:34 -0600

    Made Some changes to the project

--- Javascript/active.js
+++ Javascript/active.js
@@ -1,13 +1,44 @@
-function bookinfo(title, author, pages, readOrNot) {
+const btnADD = document.querySelector('.Add');
+const btnRemove = document.querySelector('.Removed');
+const btnRead = document.querySelector('.Read');
+const btnToRead = document.querySelector('.ToRead');
+const form = document.querySelector('.form');
+const submit = document.querySelector('.submit');
+const inputTitle = document.querySelector('#bookname');
+const inputAuthor = document.querySelector('#author');
+const inputGenre = document.querySelector('#genre');
+const inputStatus = document.querySelector('#status');
+
+const mylibrary = [
+];
+
+function book(title, author, genre, status) {
     this.title = title;
     this.author = author;
-    this.pages = pages;
-    this.readOrNot = readOrNot;
-    this.bookinfo = function() {
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readOrNot + "."}`;
+    this.genre = genre;
+    this.status = status;
+    return `${this.title} by ${this.author} is a ${this.genre} book and it is ${this.status}`;
+}
+
+function addOrRemove() {
+    if(form.style.display === 'none' || form.style.display === ''){
+        form.style.display = 'block';
     }
 }
 
-const thehobbit = new bookinfo('The Hobbit', 'J.R.R. Tolkien', 295, 'not read');
-console.log(thehobbit.bookinfo());
+function submitForm() {
+    if(form.style.display === 'block'){
+        form.style.display = 'none';
+    }
+    let title = inputTitle.value;
+    let author = inputAuthor.value;
+    let genre = inputGenre.value;
+    let status = inputStatus.value;
+    let newBook = new book(title, author, genre, status);
+    mylibrary.push(newBook);
+}
+
+function displayBooks() {
+    console.log(mylibrary);
+}
 
