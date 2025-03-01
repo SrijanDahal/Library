2c243cd6d3fc9a758e8846623dd04e8577c0abda
SrijanDahal <dahalsr@warhawks.ulm.edu>
2025-03-01 14:10:55 -0600

    Finally made the library

--- Javascript/active.js
+++ Javascript/active.js
@@ -11,8 +11,12 @@ const inputStatus = document.querySelector('#status');
 const card = document.querySelector('.cardOfBooks');
 const bookinLibrary = document.querySelector('.booksinLibrary');
 const listOfBooks = document.querySelector('.listOfBooks'); 
+const read = document.querySelector('.read');
+const remove = document.querySelector('.remove');
+const removedbooks = document.querySelector('.removedbooks');
 
 const mylibrary = [];
+const trash = [];
 
 function Book(title, author, genre, status) {
     this.title = title;
@@ -35,35 +39,78 @@ function add() {
 }
 
 
-
-let newBook1 = new Book('The Alchemist', 'Paulo Coelho', 'Fiction', 'Read');
-mylibrary.push(newBook1.info());
-let newBook2 = new Book('The Subtle Art of Not Giving a F*ck', 'Mark Manson', 'Self-help', 'To Read');
-mylibrary.push(newBook2.info());
-let newBook3 = new Book('The 48 Laws of Power', 'Robert Greene', 'Self-help', 'Read');
-mylibrary.push(newBook3.info());
-let newBook4 = new Book('The Art of War', 'Sun Tzu', 'Self-help', 'To Read');
-mylibrary.push(newBook4.info());
-let newBook5 = new Book('The Power of Now', 'Eckhart Tolle', 'Self-help', 'Read');
-mylibrary.push(newBook5.info());
-let newBook6 = new Book('The 7 Habits of Highly Effective People', 'Stephen Covey', 'Self-help', 'To Read');
-mylibrary.push(newBook6.info());
-
-
-for(let i = 0; i < mylibrary.length; i++){
+function submitForm() {
+    if(inputTitle.value === '' || inputAuthor.value === '' || inputGenre.value === '' || inputStatus.value === ''){
+        alert('Please fill in all fields');
+    }
+    else {
+        const newBook = new Book(inputTitle.value, inputAuthor.value, inputGenre.value, inputStatus.value);
+    mylibrary.push(newBook.info());
     const div = document.createElement('div');
     div.classList.add('card');
     const read = document.createElement('button');
     const remove = document.createElement('button');
     read.classList.add('read');
     remove.classList.add('remove');
-    read.textContent = 'Read';
+
+    // Checking the staus of the book //
+    if(inputStatus.checked) {
+        read.textContent = 'Read';
+    }
+    else {
+        read.textContent = 'Unread';
+    }
+
     remove.textContent = 'Remove';
-    div.innerHTML = mylibrary[i];
+    div.innerHTML = mylibrary[mylibrary.length - 1];
     listOfBooks.appendChild(div);
     div.appendChild(read);
     div.appendChild(remove);
+
+    // color of the read button//
+    if(read.textContent === 'Read'){
+        read.style.backgroundColor = 'green';
+    }
+    else if(read.textContent === 'Unread'){
+        read.style.backgroundColor = 'red';
+    }
+
+
+    // Displaying the form //
+    if(form.style.display === 'none' || form.style.display === ''){
+        form.style.display = 'block';
+    }
+    else if(form.style.display === 'block'){
+        form.style.display = 'none';
+    }
+
+    // Chaning the status of the book //
+    read.addEventListener('click', function() {
+        if(read.textContent === 'Read'){
+            read.textContent = 'Unread';
+            read.style.backgroundColor = 'red';
+        }
+        else if(read.textContent === 'Unread'){
+            read.textContent = 'Read';
+            read.style.backgroundColor = 'green';
+        }
+    });
+
+    // Removing the book from the library //
+    remove.addEventListener('click', function() {
+        div.style.display = 'none';
+        trash.push(div);
+        console.log(trash);
+    });
+
+    inputTitle.value = '';
+    inputAuthor.value = '';
+    inputGenre.value = '';
+    inputStatus.checked = false;
+    }
 }
+
+
 
 
 function displayBooks() {
